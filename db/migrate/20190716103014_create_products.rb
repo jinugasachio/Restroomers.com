060f8a5044aed5d28e5fa8dc3ae62837752d5914
Yukio Ugajin <jinugasachio@gmail.com>
2019-07-16 21:23:21 +0900

    modify

--- db/migrate/20190716103014_create_products.rb
+++ db/migrate/20190716103014_create_products.rb
@@ -1,7 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-
+      t.string :name
+      t.integer :price
+      t.text :image_url
+      t.string :manifucturer
+      t.integer :internal_capacity
       t.timestamps
     end
   end
