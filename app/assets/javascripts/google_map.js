eb8065875045f00232f20cf163f2df9cdbf3281d
Yukio Ugajin <jinugasachio@gmail.com>
2019-08-26 15:31:07 +0900

    add lip.png

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -1,11 +1,21 @@
 function initMap() {
-  //解説2：変数
+  //マップ生成のための変数
   var mapPosition = {lat: 35.658230, lng: 139.701642}; //渋谷駅スタート
   var mapArea = document.getElementById('google_map');
   var mapOptions = {
     center: mapPosition,
     zoom: 16,
   };
-  //解説1：マップを生成するMapクラス
+  //マップを生成するMapクラス
   var map = new google.maps.Map(mapArea, mapOptions);
+
+  //マーカー生成のための変数
+  var markerOptions = {
+    map: map,
+    position: mapPosition,
+    icon: "/assets/images/lip.png",
+  };
+
+  //解説1：マーカーを生成するMarkerクラス
+  var marker = new google.maps.Marker(markerOptions);
 }
