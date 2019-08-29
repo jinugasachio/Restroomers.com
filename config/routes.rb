03823c99dbfb7a3548fb53bf59ea201800759737
Yukio Ugajin <jinugasachio@gmail.com>
2019-08-29 20:31:07 +0900

    add javascript_pack_tag

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
   get 'static_pages/home'
+  get 'static_pages/vue'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
