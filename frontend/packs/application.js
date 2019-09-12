ebd270e1e47f24ae03bc82d316c0944360bb5c69
Yukio Ugajin <jinugasachio@gmail.com>
2019-09-12 12:20:05 +0900

    add gppgle_map.vue

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -3,6 +3,7 @@
 //Vue
 import Vue from 'vue'
 import App from './components/app.vue'
+import './components/google_map.vue'
 
 // import App from  './components/google_map.js'
 
---
+++ frontend/packs/components/google_map.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!!!!"
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+p {
+  font-size: 3em;
+  color: green;
+  text-align: center;
+}
+</style>
