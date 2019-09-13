c984977928ac7069dd382745c2298526bc10ca4a
Yukio Ugajin <jinugasachio@gmail.com>
2019-09-13 15:46:22 +0900

    fix google_map.vue

--- frontend/packs/components/google_map.vue
+++ frontend/packs/components/google_map.vue
@@ -10,30 +10,29 @@ export default {
 
   data: function () {
     return {
-      mapName: "map", //mapを紐付ける要素のID
-      map: null, //GoogleMapのインスタンスを入れる変数
-      markers: [], //マーカー群を入れる配列
+      mapName: "map",
+      map: null,
+      center: {lat: 35.658230, lng: 139.701642},
+      styles: gmapStyle,
+      zoom: 16,
+      markers: [],
     }
   },
 
   methods: {
-    // マップ表示メソッド
-    initMap: function () {
+    createMap: function () {
       // const my_this = this; //thisを別名で退避しておく（あとでつかうので）
+
+      // マップの生成
       const mapArea = document.getElementById("map");
-      // マップの初期値
-      const mapOptions ={
-        center: {lat: 35.658230, lng: 139.701642}, //渋谷駅スタート
-        styles: gmapStyle,
-        zoom: 16,
-      };
-      // マップ生成
-      this.map = new google.maps.Map(document.getElementById(this.mapName), mapOptions);
-      
+      const mapOptions ={center: this.center, styles: this.styles, zoom: this.zoom};
+      this.map = new google.maps.Map(mapArea, mapOptions);
+
+
     },
   },
   mounted: function () {
-    this.initMap();
+    this.createMap();
   }
 }
 
@@ -47,7 +46,6 @@ export default {
 #map {
   width: 70%;
   height: 100%;
-  // background-color: red;
 }
 
 </style>
