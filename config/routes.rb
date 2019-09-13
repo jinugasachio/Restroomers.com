d2a190a19dcd9af31738cf54a1873c29e439c1f7
Yukio Ugajin <jinugasachio@gmail.com>
2019-09-13 18:47:38 +0900

    delete files no need

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
   get 'static_pages/home'
-  get 'static_pages/vue'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- frontend/packs/components/google_map.vue
+++ frontend/packs/components/google_map.vue
@@ -1,6 +1,8 @@
 <template>
+
   <div id="map">
   </div>
+  
 </template>
 
 <script>
@@ -15,6 +17,10 @@ export default {
       styles: gmapStyle,
       zoom: 16,
       markers: [],
+      icon: {
+          url: "packs/images/woman.png",
+          scaledSize: new google.maps.Size(60, 60)
+      }
     }
   },
 
@@ -24,20 +30,15 @@ export default {
 
       // マップの生成
       const mapArea = document.getElementById("map");
-      const mapOptions ={center: this.center, styles: this.styles, zoom: this.zoom};
+      const mapOptions ={ center: this.center, styles: this.styles, zoom: this.zoom };
       this.map = new google.maps.Map(mapArea, mapOptions);
 
-      //マーカー生成のための変数
-      var markerOptions = {
-        map: this.map,
-        position: this.center, //パウダールームの住所の情報をここに持って来れば良い。つまり{lat: 35.658230, lng: 139.701642};この形の配列を渡してeachで回す？
-        icon: {
-          url: "packs/images/woman.png",
-          scaledSize: new google.maps.Size(50, 50),
-        }
-      };
-      //解説1：マーカーを生成するMarkerクラス
-      var marker = new google.maps.Marker(markerOptions);
+      //マーカー生成のための変数, 
+      // パウダールームの住所の情報(座標)をここに持って来れば良い。
+      // つまり{lat: 35.658230, lng: 139.701642};
+      // この形の配列を渡してeachで回す？
+      const markerOptions = { map: this.map, position: this.center, icon: this.icon };
+      new google.maps.Marker(markerOptions);
     },
   },
   mounted: function () {
