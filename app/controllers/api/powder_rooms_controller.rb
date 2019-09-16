2da2fd4dc75229c29f1e339c2615a841bf1851b5
Yukio Ugajin <jinugasachio@gmail.com>
2019-09-16 19:59:00 +0900

    fix powder_rooms_controller

--- app/controllers/api/powder_rooms_controller.rb
+++ app/controllers/api/powder_rooms_controller.rb
@@ -1,6 +1,10 @@
 class Api::PowderRoomsController < ApplicationController
   def index
-    @rooms = PowderRoom.all
-    render json: @rooms
+    powder_rooms = PowderRoom.all
+    render json: powder_rooms
+    # 上記のように書くことでjbuilderは必要ない。
+    # ただ条件によってhtmlを使いたい場合もあると思うので、
+    # その場合はchat_spaceの時みたいに分岐させた上で、
+    # jbuilderが必要になる。
   end
 end
