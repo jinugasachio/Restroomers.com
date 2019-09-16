5ed46e7e47570376e6e151954239d35f877d4b21
Yukio Ugajin <jinugasachio@gmail.com>
2019-09-16 17:02:49 +0900

    add route resources :powder_rooms

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
   get 'static_pages/home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    resources :powder_rooms, defaults: { format: 'json' }
+  end
 end
