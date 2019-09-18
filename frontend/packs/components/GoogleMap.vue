80942eb720b9f1956a54fcfa35b63c8301e9fae6
Yukio Ugajin <jinugasachio@gmail.com>
2019-09-18 15:02:28 +0900

    add google.maps.event.addListener in GoogleMap.vue

--- frontend/packs/components/GoogleMap.vue
+++ frontend/packs/components/GoogleMap.vue
@@ -4,21 +4,21 @@
 </template>
 
 <script>
-import gmapStyle from "../gmap_style.json"
+import gmapStyle from "../modules/gmap_style.json"
 export default {
 
   data: function () {
     return {
       mapName: "map",
       map: null,
-      center: {lat: 35.658230, lng: 139.701642}, //渋谷駅スタート
+      center: { lat: 35.658230, lng: 139.701642 }, //渋谷駅スタート
       styles: gmapStyle,
       zoom: 16,
       markers: null,
       icon: {
           url: "packs/images/woman.png",
-          scaledSize: new google.maps.Size(50, 50)
-      }
+          scaledSize: new google.maps.Size(70, 70)
+      },
     }
   },
 
@@ -51,9 +51,7 @@ export default {
 
   mounted: function() {
     this.createMap();
-    // this.addListener();
     this.pullMarkers();
-    // this.addListener();
   },
 
   watch: {
@@ -63,15 +61,12 @@ export default {
     // https://se-tomo.com/2018/10/25/vue-js%E3%81%AE%E7%9B%A3%E8%A6%96%E3%83%97%E3%83%AD%E3%83%91%E3%83%86%E3%82%A3%E3%82%A6%E3%82%A9%E3%83%83%E3%83%81%E3%83%A3/
     // マーカーの生成
     markers: function(){
+
       const vm = this
       const powderRooms = vm.markers
+      let   openWindow = null
 
       powderRooms.forEach(function(room){
-        const roomName = `<div class='room_name' data-id='${room.id}'>
-                            ${room.name}
-                          </div>`
-        const infowindow = new google.maps.InfoWindow({ content: roomName });
-
         const markerOptions = { 
           map:        vm.map, 
           position: { lat: room.lat, lng: room.lng }, 
@@ -79,21 +74,29 @@ export default {
         };
         const marker = new google.maps.Marker(markerOptions);
 
-        marker.addListener('click', function() {
-          // infowindow.close();
-          infowindow.open(map, marker);
+        // 名前から個別情報を識別できるのであればカスタムデータ属性をつける必要はない
+        // ただ名前の横に可愛いアイコンをつけたいので、そのためにはinfowindowのhtmlを
+        // カスタマイズする必要がある。ので一旦下は消さない
+
+        const roomName = `<div class='room_name' data-id='${room.id}'>
+                            ${room.name}
+                          </div>`
+        const infowindow = new google.maps.InfoWindow({ content: room.name });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          if (openWindow) {
+            openWindow.close();
+          }
           this.map.addListener('click', function(){
-            infowindow.close();
+            openWindow.close();
           })
-          // marker.addListener('click', function(){
-          //   infowindow.close();
-          // })
-
+          infowindow.open(map, marker);
+          openWindow = infowindow;
         });
-
       });
     }
   }
+
 }
 
 </script>
