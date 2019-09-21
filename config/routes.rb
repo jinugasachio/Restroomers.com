c35e20b7894ad1d21cb061746366aabf61dafd6e
Yukio Ugajin <jinugasachio@gmail.com>
2019-09-21 16:11:12 +0900

    rename home.html.erb to top.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-
   devise_for :users
-  root 'static_pages#home'
+  root 'static_pages#top'
+
+  # router.jsで指定してるパスはずべて下記のようにルートにリダイレクトさせることで
+  # エラーを防げる、今の所は一つづつ書いてくしかないかなー？
+  # もっとスマートな書き方があるはず。
+  # get 'post', to: 'static_pages#top'
 
   namespace :api do
     resources :powder_rooms, defaults: { format: 'json' }
   end
-  
 end
