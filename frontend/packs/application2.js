785f73a426580ba5ea083318863755af0da54c8b
Yukio Ugajin <jinugasachio@gmail.com>
2019-09-21 12:37:47 +0900

    remove onsenUI in npm and add it by scripttag

--- frontend/packs/application2.js
+++ frontend/packs/application2.js
@@ -1,9 +1,9 @@
-import VueOnsen from 'vue-onsenui'
+// import VueOnsen from 'vue-onsenui'
 import Vue from 'vue'
-import 'onsenui/css/onsenui.css';
-import 'onsenui/css/onsen-css-components.css';
+// import 'onsenui/css/onsenui.css';
+// import 'onsenui/css/onsen-css-components.css';
 
 Vue.use(VueOnsen)
-const a = new Vue({
+// const a = new Vue({
 
-})
+// })
