232d282567645a2116c9324ec4357c6b0d64a207
Yukio Ugajin <jinugasachio@gmail.com>
2019-09-22 15:55:32 +0900

    change TabBar.vue

--- frontend/packs/components/TabBar.vue
+++ frontend/packs/components/TabBar.vue
@@ -1,9 +1,80 @@
 <template>
-<p>読み込めている？</p>
+    <ons-tabbar swipeable position="auto"
+      :tabs="tabs"
+      :visible="true"
+      :index.sync="activeIndex"
+    >
+      <ons-tab v-for="(tab, i) in tabs"
+        :icon="tabs[i].icon"
+        :label="tabs[i].label"
+        :badge="tabs[i].badge"
+        :key= i
+      >
+      </ons-tab>
+    </ons-tabbar>
+
 </template>
 
 <script>
-// export default {
+const homePage = {
+  template: '#home',
+  props: ['myProp']
+};
 
-// }
+const newsPage = {
+  template: '#news'
+};
+
+const settingsPage = {
+  template: '#settings'
+};
+
+export default {
+  data: function() {
+    return {
+      activeIndex: 0,
+      tabs: [
+        {
+          icon: 'ion-ios-home',
+          label: '',
+          page: homePage,
+          props: {
+            myProp: 'This is a page prop!'
+          },
+          key: "homePage"
+        },
+        {
+          icon: 'packs/images/lipstick.png',
+          label: '',
+          page: newsPage,
+          badge: 7,
+          key: "newsPage"
+        },
+        {
+          icon: 'ion-ios-search',
+          label: '',
+          page: settingsPage,
+          key: "settingsPage"
+        },
+        {
+          icon: 'ion-ios-navigate',
+          label: '',
+          page: settingsPage,
+          key: "settingsPage"
+        }
+      ]
+    };
+  },
+  // methods: {
+  //   md() {
+  //     return this.$ons.platform.isAndroid();
+  //   }
+  // },
+  // computed: {
+  //   title() {
+  //     return this.tabs[this.activeIndex].label;
+  //   }
+  // }
+}
+
 </script>
