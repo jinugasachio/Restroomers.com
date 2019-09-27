d1ffcdec60a35f845cd00c01910e6bdf7d8d6c6d
Yukio Ugajin <jinugasachio@gmail.com>
2019-09-27 15:51:06 +0900

    change index method in powder_room_controller.rb

--- app/controllers/api/powder_rooms_controller.rb
+++ app/controllers/api/powder_rooms_controller.rb
@@ -1,6 +1,6 @@
 class Api::PowderRoomsController < ApplicationController
   def index
-    powder_rooms = PowderRoom.all
+    powder_rooms = PowderRoom.where(ancestry: nil) # 親のみを取り出す
     render json: powder_rooms
     # 上記のように書くことでjbuilderは必要ない。
     # ただ条件によってhtmlを使いたい場合もあると思うので、
