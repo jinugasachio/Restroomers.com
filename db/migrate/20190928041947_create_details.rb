321e5c51445354ce1dec768930e37d81909296e5
Yukio Ugajin <jinugasachio@gmail.com>
2019-09-28 14:48:33 +0900

    commit before create details_table

--- db/migrate/20190928041947_create_details.rb
+++ db/migrate/20190928041947_create_details.rb
@@ -1,7 +1,14 @@
 class CreateDetails < ActiveRecord::Migration[5.2]
   def change
     create_table :details do |t|
-
+      t.time :open
+      t.time :close
+      t.string :phone_number
+      t.string :address
+      t.text :official_url
+      t.integer :membership, default: 0
+      t.string :rate_plan
+      t.references :powder_room, foreign_key: true
       t.timestamps
     end
   end
