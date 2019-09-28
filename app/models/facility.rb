d909ce1fd99f6735481c0bdeb6c4bd66070a5b20
Yukio Ugajin <jinugasachio@gmail.com>
2019-09-28 16:05:17 +0900

    change enums in facility.rb

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,15 +1,15 @@
 class Facility < ApplicationRecord
   # Enums
-  enum dresser:         { "-": 0, "×": 1, "◯(イスあり)": 2, "◯(イスなし)": 3 }
-  enum body_mirror:     { "-": 0, "×": 1, "◯": 2 }
-  enum makeup_mirror:   { "-": 0, "×": 1, "◯": 2 }
-  enum wifi:            { "-": 0, "×": 1, "◯": 2 }
-  enum fitting_booth:   { "-": 0, "×": 1, "◯": 2 }
-  enum washstands:      { "-": 0, "×": 1, "◯": 2 }
-  enum luggage_storage: { "-": 0, "×": 1, "◯": 2 }
-  enum outlet:          { "-": 0, "×": 1, "◯": 2 }
-  enum dust_box:        { "-": 0, "×": 1, "◯": 2 }
-  enum waiting_sofa:    { "-": 0, "×": 1, "◯": 2 }
+  enum dresser:         { "--": 0, "×": 1, "◯(イスあり)": 2, "◯(イスなし)": 3 }
+  enum body_mirror:     { "--": 0, "×": 1, "◯": 2 }
+  enum makeup_mirror:   { "--": 0, "×": 1, "◯": 2 }
+  enum wifi:            { "--": 0, "×": 1, "◯": 2 }
+  enum fitting_booth:   { "--": 0, "×": 1, "◯": 2 }
+  enum washstands:      { "--": 0, "×": 1, "◯": 2 }
+  enum luggage_storage: { "--": 0, "×": 1, "◯": 2 }
+  enum outlet:          { "--": 0, "×": 1, "◯": 2 }
+  enum dust_box:        { "--": 0, "×": 1, "◯": 2 }
+  enum waiting_sofa:    { "--": 0, "×": 1, "◯": 2 }
 
   # Association
   belongs_to :powder_room
