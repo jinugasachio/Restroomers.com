baa10c7dcca456807838f3719638332996133370
Yukio Ugajin <jinugasachio@gmail.com>
2019-09-29 23:28:07 +0900

    clean PowderRoom.vue

--- frontend/packs/components/PowderRoom.vue
+++ frontend/packs/components/PowderRoom.vue
@@ -20,16 +20,24 @@
         <v-ons-list>
           <v-ons-list-header>詳細情報</v-ons-list-header>
           <v-ons-list-item>営業時間
-            <div class="right">{{ room.open }}~{{ room.close }}</div>
+            <div class="right">
+              {{ room.open }} ~ {{ room.close }}
+            </div>
           </v-ons-list-item>
           <v-ons-list-item>TEL
-            <a class="right" :href="'tel:' + room.phone_number">{{ room.phone_number }}</a>
+            <a class="right" :href="'tel:' + room.phone_number">
+              {{ room.phone_number }}
+            </a>
           </v-ons-list-item>
           <v-ons-list-item>住所
-            <div class="right">{{ room.address }}</div>
+            <div class="right">
+              {{ room.address }}
+            </div>
             </v-ons-list-item>
           <v-ons-list-item>公式HP
-            <a class="right" target="_blank" :href=room.official_url>{{ room.official_url }}</a>
+            <a class="right" target="_blank" :href=room.official_url>
+              {{ room.official_url }}
+            </a>
           </v-ons-list-item>
         </v-ons-list>
       </div>
@@ -53,3 +61,12 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+
+a {
+  &:hover {
+    cursor: pointer !important;
+  }
+}
+</style>
