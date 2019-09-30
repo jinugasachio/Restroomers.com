a66b33014293e2cea548f269045f79f81244306f
Yukio Ugajin <jinugasachio@gmail.com>
2019-09-30 15:11:21 +0900

    change style in PowderRoom.vue

--- frontend/packs/components/PowderRoom.vue
+++ frontend/packs/components/PowderRoom.vue
@@ -23,55 +23,55 @@
           <div class="f-container">
             <div class="f-item">
               <p>ドレッサー</p>
-              <div><p>○</p></div>
+              <div><p>{{ room.dresser }}</p></div>
             </div>
             <div class="f-item">
               <p>全身鏡</p>
-              <div><p>○</p></div>
+              <div><p>{{ room.body_mirror }}</p></div>
             </div>
             <div class="f-item">
               <p>拡大鏡</p>
-              <div><p>○</p></div>
+              <div><p>{{ room.makeup_mirror }}</p></div>
             </div>
             <div class="f-item">
               <p>無料Wi-Fi</p>
-              <div><p>○</p></div>
+              <div><p>{{ room.wifi }}</p></div>
             </div>
             <div class="f-item">
               <p>フィッティングスペース</p>
-              <div><p>○</p></div>
+              <div><p>{{ room.fitting_booth }}</p></div>
             </div>
             <div class="f-item">
               <p>手洗いボウル</p>
-              <div><p>○</p></div>
+              <div><p>{{ room.washstands }}</p></div>
             </div>
             <div class="f-item">
               <p>荷物置き</p>
-              <div><p>○</p></div>
+              <div><p>{{ room.luggage_storage }}</p></div>
             </div>
             <div class="f-item">
               <p>コンセント</p>
-              <div><p>○</p></div>
+              <div><p>{{ room.outlet }}</p></div>
             </div>
             <div class="f-item">
               <p>ウェイティングスペース</p>
-              <div><p>○</p></div>
+              <div><p>{{ room.waiting_space }}</p></div>
             </div>
             <div class="f-item">
               <p>ゴミ箱</p>
-              <div><p>○</p></div>
+              <div><p>{{ room.dust_box }}</p></div>
             </div>
             <div class="f-item">
               <p>利用条件</p>
-              <div><p>○</p></div>
+              <div><p class="letter">{{ room.membership }}</p></div>
             </div>
             <div class="f-item">
               <p>料金プラン</p>
-              <div><p>○</p></div>
+              <div><p class="letter">{{ room.rate_plan }}</p></div>
             </div>
             <div class="f-item others">
               <p>その他</p>
-              <div><p>○</p></div>
+              <div><p class="letter">{{ room.others }}</p></div>
             </div>
           </div>
         </v-ons-list>
@@ -148,10 +148,20 @@ a {
 
     &.others {
       width: 97%;
+
+      .letter {
+        padding: 1rem 1rem;
+        text-align: left;
+      }
     }
 
     div {
       border-top: solid  #b2b2b2 1px;
+
+      .letter {
+        padding: 0.5rem 0;
+        font-size: 1rem;
+      }
 
       p {
         font-size: 2rem;
