157f017c92f0ccb80df0b58ad38d93c23a371906
Yukio Ugajin <jinugasachio@gmail.com>
2019-10-01 11:36:41 +0900

    add images for pr11

---
+++ db/seeds/image.rb
@@ -0,0 +1,6 @@
+require 'open-uri'
+
+Image.create(
+  urls: "#{Rails.root}/db/images/pr11/pr11-1.jpg",
+  powder_room_id: 11
+)
--- frontend/packs/components/Review.vue
+++ frontend/packs/components/Review.vue
@@ -12,7 +12,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
 .list-header {
   margin-top: 1rem;
 }
+
 </style>
