9ec18b159c1ee328dbbb01a6b751a493656d915b
Yukio Ugajin <jinugasachio@gmail.com>
2019-10-01 15:01:03 +0900

    change PowderRoomDetai.vue

--- frontend/packs/components/PowderRoomDetail.vue
+++ frontend/packs/components/PowderRoomDetail.vue
@@ -3,21 +3,21 @@
     <v-ons-list-header>詳細情報</v-ons-list-header>
     <v-ons-list-item>営業時間
       <div class="right">
-        {{ room.open }} ~ {{ room.close }}
+        {{ detail.open }} ~ {{ detail.close }}
       </div>
     </v-ons-list-item>
     <v-ons-list-item>TEL
-      <a class="right" :href="'tel:' + room.phone_number">
-        {{ room.phone_number }}
+      <a class="right" :href="'tel:' + detail.phone_number">
+        {{ detail.phone_number }}
       </a>
     </v-ons-list-item>
     <v-ons-list-item>住所
       <div class="right">
-        {{ room.address }}
+        {{ detail.address }}
       </div>
       </v-ons-list-item>
     <v-ons-list-item>公式HP
-      <a class="right" target="_blank" :href=room.official_url>
+      <a class="right" target="_blank" :href=detail.official_url>
         ホームページを確認する
       </a>
     </v-ons-list-item>
@@ -27,8 +27,8 @@
 <script>
 export default {
   computed: {
-    room(){
-      return this.$store.getters.powderRoom
+    detail(){
+      return this.$store.getters.powderRoom.detail
     },
   },
 }
