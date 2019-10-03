1e114064c5bdd55145a0ed9bc8e6d5f39d448987
Yukio Ugajin <jinugasachio@gmail.com>
2019-10-03 19:13:21 +0900

    change seeds/image.rb

--- db/seeds/image.rb
+++ db/seeds/image.rb
@@ -1,10 +1,16 @@
 
 
-image_dir_array = ['./db/images/pr1/*']
-urls = []
+# image_dir_array = ['./db/images/pr1/*', './db/images/pr2/*']
+
+image_dir_array = []
+25.times do |num|
+  image_dir_array.push("./db/images/pr#{num + 1}/*")
+end
+
 image_dir_array.each.with_index(1) do |dir, n|
+  files = []
   Dir.glob(dir).each do |image|
-    urls.push(File.open(image))
+    files.push(File.open(image))
   end
-  Image.create(urls: urls, powder_room_id: n)
+  Image.create(urls: files, powder_room_id: n)
 end
