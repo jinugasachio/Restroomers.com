b523cb1f31081caf08126cd7560a9099be0b2be3
Yukio Ugajin <jinugasachio@gmail.com>
2019-10-03 21:56:32 +0900

    change carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,18 +10,18 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ,
-      aws_secret_access_key: ,
-      region: ,
-      host:
+      aws_access_key_id: Rails.application.credentials.production[:aws][:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.production[:aws][:secret_access_key],
+      region: Rails.application.credentials.production[:aws][:s3_region],
+      host: Rails.application.credentials.production[:aws][:s3_host]
     }
 
     # cacheもS3に置く
     config.cache_storage = :fog
     config.cache_dir = 'tmp/image-cache'
 
-    config.fog_directory  = 
+    config.fog_directory = Rails.application.credentials.production[:aws][:s3_backet]
 
-    config.asset_host = 
+    config.asset_host = Rails.application.credentials.production[:aws][:fog_asset_host]
   end
 end
