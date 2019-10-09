597b4af5d99f21bf0d08c8c6d968c8bb757f9f2b
Yukio Ugajin <jinugasachio@gmail.com>
2019-10-09 21:12:42 +0900

    add SearchBox.vue to Modal.vue

--- frontend/packs/components/Modal.vue
+++ frontend/packs/components/Modal.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-ons-modal :visible="true" @click="modalVisible = false">
+    <v-ons-modal :visible="showSearchBox">
       <SearchBox/>
     </v-ons-modal>
 </template>
@@ -10,17 +10,20 @@ import SearchBox from './SearchBox.vue'
 
 export default {
   name: 'Modal',
-  data(){
-    return{
-      modalVisible: false
-    }
-  },
+
   components: {
     SearchBox
-  }
+  },
+  computed: {
+    showSearchBox(){
+      return this.$store.getters.showSearchBox
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-  
+  .modal {
+    bottom: 49px;
+  }
 </style>
--- frontend/packs/store.js
+++ frontend/packs/store.js
@@ -17,7 +17,7 @@ const store =  new Vuex.Store({
     pageStack: [GoogleMap],
     directionTrigger: false,
     guideTrigger: false,
-    showSearchBox: true,
+    showSearchBox: false,
 
   },
 
