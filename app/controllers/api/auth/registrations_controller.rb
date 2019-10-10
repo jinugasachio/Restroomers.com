d03b325ccfa12a9952a01ca183edc922a339b031
Yukio Ugajin <jinugasachio@gmail.com>
2019-10-10 18:59:11 +0900

    add private params to resistrations controller

--- app/controllers/api/auth/registrations_controller.rb
+++ app/controllers/api/auth/registrations_controller.rb
@@ -1,2 +1,10 @@
 class Api::Auth::RegistrationsController < ApplicationController
+  private
+  def sign_up_params
+    params.permit(:nickname, :email, :image, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.permit(:nickname, :email, :image)
+  end
 end
