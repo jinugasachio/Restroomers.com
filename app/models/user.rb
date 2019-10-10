deb3af6d3f7f2fc84df60899be6db752545ad928
Yukio Ugajin <jinugasachio@gmail.com>
2019-10-10 18:27:42 +0900

    rails g devise_token_auth:install User auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable
+          # :confirmable, :omniauthable
+  include DeviseTokenAuth::Concerns::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
