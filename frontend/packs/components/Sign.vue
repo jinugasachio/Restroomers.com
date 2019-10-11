0147684b34d34e6a9e93dbc3fa3670ed47930a4c
Yukio Ugajin <jinugasachio@gmail.com>
2019-10-11 11:15:32 +0900

    change style in Sign.vue

--- frontend/packs/components/Sign.vue
+++ frontend/packs/components/Sign.vue
@@ -1,13 +1,15 @@
 <template>
     <v-ons-page>
       <ToolBar/>
+      <div class="wrapper">
       <v-ons-card>
         <h1 class="title">Restroomers.com</h1>
-        <p class="title-text">お気に入りのレストルームを見つけよう！</p>
-        <v-ons-button modifier="large" id="sign_up" >登録する</v-ons-button>
+        <p class="title-text">近くのレストルームを見つけよう！</p>
+        <v-ons-button modifier="large" id="sign_up" >新規登録</v-ons-button>
         <v-ons-button modifier="large" id="sign_in">ログイン</v-ons-button>
         <v-ons-button modifier="large" id="sign_in-easy" >簡単ログイン</v-ons-button>
       </v-ons-card>
+      </div>
     </v-ons-page>
 </template>
 
@@ -28,15 +30,46 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.card {
-  .title {
-    text-align: center;
-  }
 
-  .title-text {
-    font-weight: 900;
-    color: #7a7a7a;
-    text-align: center;
+.wrapper {
+  display: flex;
+  width: 100%;
+  height: 100%;
+
+  .card {
+    display: inline-block;
+    width: 90%;
+    max-width: 560px;
+    margin: auto;
+
+    .title {
+      text-align: center;
+    }
+
+    .title-text {
+      margin-bottom: 2rem;
+      font-size: 0.9rem;
+      font-weight: 900;
+      color: #7a7a7a;
+      text-align: center;
+    }
+
+    .button--large {
+      width: 80%;
+      margin: 0 auto 2rem;
+    }
+
+    #sign_up {
+      background-color: #ff8b85;
+    }
+
+    #sign_in {
+      background-color: #ff8b85;
+    }
+
+    #sign_in-easy {
+      background-color: #ff554D;
+    }
   }
 }
 </style>
