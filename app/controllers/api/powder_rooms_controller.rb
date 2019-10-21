5f6ef4fb3c0250ad25fa1ec5e13d369c415dfc4b
Yukio Ugajin <jinugasachio@gmail.com>
2019-10-21 18:21:18 +0900

    add create method and strong_parameter to powder_room_controller

--- app/controllers/api/powder_rooms_controller.rb
+++ app/controllers/api/powder_rooms_controller.rb
@@ -1,4 +1,6 @@
 class Api::PowderRoomsController < ApplicationController
+  before_action :authenticate_api_user!, only: :create
+
   def index
     powder_rooms = PowderRoom.where(ancestry: nil) # 親のみを取り出す
     render json: powder_rooms
@@ -26,4 +28,48 @@ class Api::PowderRoomsController < ApplicationController
       render json: children
     end
   end
+
+  def create
+    room = current_api_user.powder_room.new(params[:room_params])
+    room.build_images(params[:images_params])
+    room.build_facility(params[:facility_params])
+    room.build_detail(params[:details_params])
+    room.save
+    render json: room
+  end
+
+  private
+
+    def params
+      params.require(:room).permit(
+        room_params: [
+          :name,
+          :lat,
+          :lng,
+        ],
+        images_params: [:urls],
+        facility_params: [
+          :dresser,
+          :body_mirror,
+          :makeup_mirror,
+          :wifi,
+          :fitting_booth,
+          :washstands,
+          :luggage_storage,
+          :outlet,
+          :dust_box,
+          :waiting_space,
+          :others,
+          :membership,
+          :rate_plan
+        ],
+        detail_params: [
+          :open,
+          :close,
+          :phone_number,
+          :address,
+          :official_url
+        ]
+      )
+    end
 end
--- frontend/packs/components/roomForm.vue
+++ frontend/packs/components/roomForm.vue
@@ -5,8 +5,8 @@
         <ImageForm/>
         <FacilityForm/>
         <DetailForm/>
-        <v-ons-button
-          modifier="large"
+        <v-ons-button id="post-button" modifier="large"
+          @click="postRoom"
         >
           投稿する
         </v-ons-button>
@@ -29,6 +29,24 @@ export default {
     FacilityForm,
     DetailForm
   },
+
+  methods: {
+    postRoom(){
+      this.$ons.notification.confirm({ message: '投稿してもよろしいですか?', title: '' })
+    }
+  }
   
 }
 </script>
+
+<style scoped lang="scss">
+
+#post-button {
+  width: 65%;
+  margin: 30px auto 10px;
+  background-color: #ff8585;
+  border-bottom: solid 2px #b5b5b5;
+  box-shadow: inset 0 2px 0 #ffffff80, 0 2px 2px #00000030;
+}
+
+</style>
