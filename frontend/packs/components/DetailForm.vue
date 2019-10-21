6240ebcfd0ae40724e7e8a3def087a9ca2fd4127
Yukio Ugajin <jinugasachio@gmail.com>
2019-10-21 13:38:40 +0900

    fix data in DetailForm.vue

--- frontend/packs/components/DetailForm.vue
+++ frontend/packs/components/DetailForm.vue
@@ -2,13 +2,15 @@
   <v-ons-card class="detail">
       <v-ons-list-header>詳細情報 (任意)</v-ons-list-header>
       <v-ons-list-item class="detail__list"
-        v-for="detail in detailForm"
-        :key="detail.name" 
+        v-for="d in detailForm"
+        :key="d.name" 
       >
-        <label v-text="detail.name" class="center">
+        <label v-text="d.name" class="center">
         </label>              
         <label class="right">
-          <v-ons-input :placeholder="detail.holder" type="text"
+          <v-ons-input type="text"
+            v-model="d.model"
+            :placeholder="d.holder"
           >
           </v-ons-input>
         </label>
@@ -25,13 +27,26 @@ export default {
   data(){
     return{
       detailForm: [
-        { name: '営業開始', holder: '例) 10:00"' },
-        { name: '営業終了', holder: '例) 21:00"' },
-        { name: 'TEL',    holder: '例) 0312345678' },
-        { name: '住所',    holder: '例) 東京都渋谷区~' },
-        { name: '公式HP',  holder: '例) https://~' }
-      ]
+        { name: '営業開始', holder: '例) 10:00"',      model: ""},
+        { name: '営業終了', holder: '例) 21:00"',      model: ""},
+        { name: 'TEL',    holder: '例) 0312345678',  model: ""},
+        { name: '住所',    holder: '例) 東京都渋谷区~', model: ""},
+        { name: '公式HP',  holder: '例) https://~',   model: ""}
+      ],
+      // detailParams: {
+      //   "open":         this.detailForm[0].model,
+      //   "close":        this.detailForm[1].model,
+      //   "phone_number": this.detailForm[2].model,
+      //   "address":      this.detailForm[3].model,
+      //   "official_url": this.detailForm[4].model
+      // }
     }
+  },
+  mounted(){
+    // debugger;
+  },
+  updated(){
+    
   }
   
 }
