34cf06d54d9337747a1133eb5a4b6f4fcc1713d0
Yukio Ugajin <jinugasachio@gmail.com>
2019-10-25 19:49:28 +0900

    divide store.js into room.js

--- frontend/packs/store.js
+++ frontend/packs/store.js
@@ -4,24 +4,21 @@ import Vuex from 'vuex'
 import GoogleMap from './components/others/GoogleMap.vue'
 import SignTop from './components/others/SignTop.vue'
 import RoomList from './components/room/RoomList.vue'
-import defaultData from './modules/default_data.json'
+import roomStore from './stores/room.js'
 
 Vue.use(Vuex)
 
+
 const store =  new Vuex.Store({
+
+  modules: {
+    roomStore
+  },
 
   state: {
 
     map: null,
     center: null,
-    room: defaultData, //コンソールエラー防止のため | リレーションしてるモデルデータも合わせて格納している
-    room_1: defaultData,
-    roomLikes:[], //リアクティブにするにはroomと分ける必要がある
-    roomLikes_1:[],
-    roomReviews:[], //リアクティブにするにはroomと分ける必要がある
-    roomReviews_1:[],
-    allRooms: [],
-    roomList: [],
     pageStack1: [GoogleMap],
     pageStack2: [SignTop],
     directionTrigger: false,
@@ -30,7 +27,6 @@ const store =  new Vuex.Store({
     postTrriger: false,
     activeTab: 0,
     currentUser: null,
-    favoriteRooms: [],
     headers: null,
     signFormData: []
 
@@ -43,36 +39,36 @@ const store =  new Vuex.Store({
     },
     room(state) {
       if (state.activeTab == 0){
-        return state.room
+        return state.roomStore.room
       }
       else if (state.activeTab == 1){
-        return state.room_1
+        return state.roomStore.room_1
       }
     },
     roomLikes(state){
       if (state.activeTab == 0){
-        return state.roomLikes;
+        return state.roomStore.roomLikes;
       }
       else if (state.activeTab == 1){
-        return state.roomLikes_1;
+        return state.roomStore.roomLikes_1;
       }
     },
     roomReviews(state){
       if (state.activeTab == 0){
-        return state.roomReviews;
+        return state.roomStore.roomReviews;
       }
       else if (state.activeTab == 1){
-        return state.roomReviews_1;
+        return state.roomStore.roomReviews_1;
       }
     },
     allRooms(state) {
-      return state.allRooms;
+      return state.roomStore.allRooms;
     },
     roomList(state) {
-      return state.roomList;
+      return state.roomStore.roomList;
     },
     favoriteRooms(state){
-      return state.favoriteRooms;
+      return state.roomStore.favoriteRooms;
     },
     pageStack(state){
       if(state.activeTab == 0){
@@ -116,90 +112,6 @@ const store =  new Vuex.Store({
 
     updateMap(state, payload) {
       state.map = payload
-    },
-    updateRoom(state, payload) {
-      if(state.activeTab == 0){
-        state.room = payload.room;
-      }
-      else if(state.activeTab == 1){
-        state.room_1 = payload.room;
-      }
-    },
-    roomLikes(state, payload){
-      if(state.activeTab == 0){
-        state.roomLikes = payload.roomLikes;
-      }
-      else if(state.activeTab == 1){
-        state.roomLikes_1 = payload.roomLikes;
-      }
-    },
-    addLike(state, payload){
-      if(state.activeTab == 0){
-        state.roomLikes.push(payload.newLike);
-      }
-      else if(state.activeTab == 1){
-        state.roomLikes_1.push(payload.newLike);
-      }
-    },
-    deleteLike(state, payload){
-      if(state.activeTab == 0){
-        state.roomLikes = state.roomLikes.filter(function(like){
-          return like.id !== payload.like.id
-        })
-      }
-      else if(state.activeTab == 1){
-        state.roomLikes_1 = state.roomLikes_1.filter(function(like){
-          return like.id !== payload.like.id
-        })
-      }
-    },
-    roomReviews(state, payload){
-      if(state.activeTab == 0){
-        state.roomReviews = payload.roomReviews;
-      }
-      else if(state.activeTab == 1){
-        state.roomReviews_1 = payload.roomReviews;
-      }
-    },
-    postReview(state, payload){
-      if(state.activeTab == 0){
-        state.roomReviews.push(payload.newReview);
-      }
-      else if(state.activeTab == 1){
-        state.roomReviews_1.push(payload.newReview);
-      }
-    },
-    deleteReview(state, payload){
-      if(state.activeTab == 0){
-        state.roomReviews = state.roomReviews.filter(function(review){
-          return review.id !== payload.review.id
-        })
-      }
-      else if(state.activeTab == 1){
-        state.roomReviews_1 = state.roomReviews_1.filter(function(review){
-          return review.id !== payload.review.id
-        })
-      }
-    },
-    favoriteRooms(state, payload){
-      state.favoriteRooms = payload.favoriteRooms;
-    },
-    addFavorite(state, payload){
-      state.favoriteRooms.push(payload)
-    },
-    deleteFavorite(state, payload){
-      state.favoriteRooms = state.favoriteRooms.filter(function(room){
-        return room.id !== payload.id
-      })
-    },
-    updateAllRooms(state, payload) {
-      state.allRooms = payload.allRooms;
-    },
-    updateRoomList(state, payload) {
-      state.roomList = payload.roomList;
-    },
-    resetRoomList(state) {
-      state.roomList = [];
     },
     pushPage(state, payload) {
       if(state.activeTab == 0){
@@ -266,9 +178,6 @@ const store =  new Vuex.Store({
     updateCenter(state, payload){
       state.center = payload;
     },
-    postRoom(state, payload){
-      state.allRooms.push(payload);
-    }
 
   },
 
@@ -276,36 +185,6 @@ const store =  new Vuex.Store({
 
     updateMap(context, map){
       context.commit('updateMap', map)
-    },
-
-    getAllRooms(context){
-      axios.get("/api/powder_rooms")
-      .then(function(response){
-        context.commit('updateAllRooms', { allRooms: response.data })
-      })
-      .catch(function (error) {
-        alert(error);
-      })
-    },
-
-    getRoom(context, url){
-      axios.get('/api/powder_rooms/' + url)
-      .then(function(response){
-        if (response.data.length > 1){
-          context.commit('updateRoomList', { roomList: response.data })
-        } else {
-          context.commit('updateRoom', { room: response.data })
-          context.commit('roomLikes',  { roomLikes: response.data.likes })
-          context.commit('roomReviews',  { roomReviews: response.data.reviews })
-        }
-      })
-      .catch(function (error) {
-        alert(error);
-      })
-    },
-
-    resetRoomList(context){
-      context.commit('resetRoomList');
     },
 
     pushPage(context, page){
@@ -380,75 +259,9 @@ const store =  new Vuex.Store({
       context.commit('showUserPage');
     },
 
-    like(context, likeParams){
-      axios.post('/api/likes', likeParams, { headers: context.state.headers })
-      .then(function(response){
-        const favoriteRoom = context.getters.room.powder_room
-        context.commit('addLike', { newLike: response.data })
-        context.commit('addFavorite', favoriteRoom)
-      })
-      .catch(function () {
-        alert('予期しないエラーが発生しました。');
-      })
-    },
-
-    unlike(context, params){
-      axios.delete('/api/likes/' + params.id,  { headers: context.state.headers })
-      .then(function(response){
-        const favoriteRoom = context.getters.room.powder_room
-        context.commit('deleteLike', { like: response.data })
-        context.commit('deleteFavorite', favoriteRoom)
-      })
-      .catch(function (error) {
-        alert('予期しないエラーが発生しました。');
-      })
-    },
-
-    favoriteRooms(context){
-      axios.get('/api/likes/favorite_rooms', { headers: context.state.headers })
-      .then(function(response){
-        context.commit('favoriteRooms', { favoriteRooms: response.data })
-      })
-      .catch(function (error) {
-        alert('予期しないエラーが発生しました。');
-      })
-    },
-
-    postReview(context, reviewParams){
-      debugger;
-      axios.post('/api/reviews', reviewParams, { headers: context.state.headers })
-      .then(function(response){
-        context.commit('postReview', { newReview: response.data })
-      })
-      .catch(function (error) {
-        alert('予期しないエラーが発生しました。');
-      })
-    },
-
-    deleteReview(context, params){
-      axios.delete('/api/reviews/' + params.id,  { headers: context.state.headers })
-      .then(function(response){
-        context.commit('deleteReview', { review: response.data })
-      })
-      .catch(function (error) {
-        alert('予期しないエラーが発生しました。');
-      })
-    },
-
-    postRoom(context, roomParams){
-      axios.post('/api/powder_rooms', roomParams,  { headers: context.state.headers })
-      .then(function(response){
-        context.commit ('postRoom', response.data)
-      })
-      .catch(function (error) {
-        alert('予期しないエラーが発生しました。');
-      })
-    },
-
     updateCenter(context, latLng){
       context.commit('updateCenter', latLng);
     }
-
   },
 
 })
---
+++ frontend/packs/stores/room.js
@@ -0,0 +1,210 @@
+import defaultData from '../modules/default_data.json'
+
+export default {
+
+  state: {
+    room: defaultData,
+    room_1: defaultData,
+    roomLikes:[],
+    roomLikes_1:[],
+    roomReviews:[],
+    roomReviews_1:[],
+    allRooms: [],
+    roomList: [],
+    favoriteRooms: [],
+    activeTab: 0,
+  },
+
+
+  mutations: {
+    updateRoom(state, payload) {
+      if(state.activeTab == 0){
+        state.room = payload.room;
+      }
+      else if(state.activeTab == 1){
+        state.room_1 = payload.room;
+      }
+    },
+    roomLikes(state, payload){
+      if(state.activeTab == 0){
+        state.roomLikes = payload.roomLikes;
+      }
+      else if(state.activeTab == 1){
+        state.roomLikes_1 = payload.roomLikes;
+      }
+    },
+    addLike(state, payload){
+      if(state.activeTab == 0){
+        state.roomLikes.push(payload.newLike);
+      }
+      else if(state.activeTab == 1){
+        state.roomLikes_1.push(payload.newLike);
+      }
+    },
+    deleteLike(state, payload){
+      if(state.activeTab == 0){
+        state.roomLikes = state.roomLikes.filter(function(like){
+          return like.id !== payload.like.id
+        })
+      }
+      else if(state.activeTab == 1){
+        state.roomLikes_1 = state.roomLikes_1.filter(function(like){
+          return like.id !== payload.like.id
+        })
+      }
+    },
+    roomReviews(state, payload){
+      if(state.activeTab == 0){
+        state.roomReviews = payload.roomReviews;
+      }
+      else if(state.activeTab == 1){
+        state.roomReviews_1 = payload.roomReviews;
+      }
+    },
+    postReview(state, payload){
+      if(state.activeTab == 0){
+        state.roomReviews.push(payload.newReview);
+      }
+      else if(state.activeTab == 1){
+        state.roomReviews_1.push(payload.newReview);
+      }
+    },
+    deleteReview(state, payload){
+      if(state.activeTab == 0){
+        state.roomReviews = state.roomReviews.filter(function(review){
+          return review.id !== payload.review.id
+        })
+      }
+      else if(state.activeTab == 1){
+        state.roomReviews_1 = state.roomReviews_1.filter(function(review){
+          return review.id !== payload.review.id
+        })
+      }
+    },
+    favoriteRooms(state, payload){
+      state.favoriteRooms = payload.favoriteRooms;
+    },
+    addFavorite(state, payload){
+      state.favoriteRooms.push(payload)
+    },
+    deleteFavorite(state, payload){
+      state.favoriteRooms = state.favoriteRooms.filter(function(room){
+        return room.id !== payload.id
+      })
+    },
+    updateAllRooms(state, payload) {
+      state.allRooms = payload.allRooms;
+    },
+    updateRoomList(state, payload) {
+      state.roomList = payload.roomList;
+    },
+    resetRoomList(state) {
+      state.roomList = [];
+    },
+    postRoom(state, payload){
+      state.allRooms.push(payload);
+    },
+    activeTab(state, payload){
+      state.activeTab = payload;
+    },
+
+  },
+
+
+  actions: {
+    getAllRooms(context){
+      axios.get("/api/powder_rooms")
+      .then(function(response){
+        context.commit('updateAllRooms', { allRooms: response.data })
+      })
+      .catch(function (error) {
+        alert('予期しないエラーが発生しました。');
+      })
+    },
+
+    getRoom(context, url){
+      axios.get('/api/powder_rooms/' + url)
+      .then(function(response){
+        if (response.data.length > 1){
+          context.commit('updateRoomList', { roomList: response.data })
+        } else {
+          context.commit('updateRoom',  { room: response.data })
+          context.commit('roomLikes',   { roomLikes: response.data.likes })
+          context.commit('roomReviews', { roomReviews: response.data.reviews })
+        }
+      })
+      .catch(function (error) {
+        alert('予期しないエラーが発生しました。');
+      })
+    },
+
+    resetRoomList(context){
+      context.commit('resetRoomList');
+    },
+
+    like(context, likeParams){
+      axios.post('/api/likes', likeParams, { headers: context.rootState.headers })
+      .then(function(response){
+        const favoriteRoom = context.rootGetters.room.powder_room
+        context.commit('addLike', { newLike: response.data })
+        context.commit('addFavorite', favoriteRoom)
+      })
+      .catch(function () {
+        alert('予期しないエラーが発生しました。');
+      })
+    },
+
+    unlike(context, params){
+      axios.delete('/api/likes/' + params.id,  { headers: context.rootState.headers })
+      .then(function(response){
+        const favoriteRoom = context.rootGetters.room.powder_room
+        context.commit('deleteLike', { like: response.data })
+        context.commit('deleteFavorite', favoriteRoom)
+      })
+      .catch(function (error) {
+        alert('予期しないエラーが発生しました。');
+      })
+    },
+
+    favoriteRooms(context){
+      debugger
+      axios.get('/api/likes/favorite_rooms', { headers: context.rootState.headers })
+      .then(function(response){
+        context.commit('favoriteRooms', { favoriteRooms: response.data })
+      })
+      .catch(function (error) {
+        alert('予期しないエラーが発生しました。');
+      })
+    },
+
+    postReview(context, reviewParams){
+      axios.post('/api/reviews', reviewParams, { headers: context.rootState.headers })
+      .then(function(response){
+        context.commit('postReview', { newReview: response.data })
+      })
+      .catch(function (error) {
+        alert('予期しないエラーが発生しました。');
+      })
+    },
+
+    deleteReview(context, params){
+      axios.delete('/api/reviews/' + params.id,  { headers: context.rootState.headers })
+      .then(function(response){
+        context.commit('deleteReview', { review: response.data })
+      })
+      .catch(function (error) {
+        alert('予期しないエラーが発生しました。');
+      })
+    },
+
+    postRoom(context, roomParams){
+      axios.post('/api/powder_rooms', roomParams,  { headers: context.rootState.headers })
+      .then(function(response){
+        context.commit ('postRoom', response.data)
+      })
+      .catch(function (error) {
+        alert('予期しないエラーが発生しました。');
+      })
+    },
+  }
+}
