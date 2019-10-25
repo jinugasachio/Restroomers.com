10f53ea81418200b4dcd619afa8b53eafd4202b6
Yukio Ugajin <jinugasachio@gmail.com>
2019-10-25 21:28:56 +0900

    add review.js

--- frontend/packs/stores/room/room.js
+++ frontend/packs/stores/room/room.js
@@ -5,21 +5,24 @@ import likeStore from './like.js'
 export default {
 
   modules: {
+
     reviewStore,
     likeStore
+
   },
 
   state: {
+
     room: defaultData,
     room_1: defaultData,
-    roomReviews:[],
-    roomReviews_1:[],
     allRooms: [],
     roomList: [],
     activeTab: 0,
+
   },
 
   getters: {
+
     room(state) {
       if (state.activeTab == 0){
         return state.room
@@ -28,58 +31,24 @@ export default {
         return state.room_1
       }
     },
-    roomReviews(state){
-      if (state.activeTab == 0){
-        return state.roomReviews;
-      }
-      else if (state.activeTab == 1){
-        return state.roomReviews_1;
-      }
-    },
     allRooms(state) {
       return state.allRooms;
     },
     roomList(state) {
       return state.roomList;
     },
+
   },
 
 
   mutations: {
+
     updateRoom(state, payload) {
       if(state.activeTab == 0){
         state.room = payload.room;
       }
       else if(state.activeTab == 1){
         state.room_1 = payload.room;
-      }
-    },
-    roomReviews(state, payload){
-      if(state.activeTab == 0){
-        state.roomReviews = payload.roomReviews;
-      }
-      else if(state.activeTab == 1){
-        state.roomReviews_1 = payload.roomReviews;
-      }
-    },
-    postReview(state, payload){
-      if(state.activeTab == 0){
-        state.roomReviews.push(payload.newReview);
-      }
-      else if(state.activeTab == 1){
-        state.roomReviews_1.push(payload.newReview);
-      }
-    },
-    deleteReview(state, payload){
-      if(state.activeTab == 0){
-        state.roomReviews = state.roomReviews.filter(function(review){
-          return review.id !== payload.review.id
-        })
-      }
-      else if(state.activeTab == 1){
-        state.roomReviews_1 = state.roomReviews_1.filter(function(review){
-          return review.id !== payload.review.id
-        })
       }
     },
     updateAllRooms(state, payload) {
@@ -102,6 +71,7 @@ export default {
 
 
   actions: {
+
     getAllRooms(context){
       axios.get("/api/powder_rooms")
       .then(function(response){
@@ -132,27 +102,6 @@ export default {
       context.commit('resetRoomList');
     },
 
-    postReview(context, reviewParams){
-      debugger;
-      axios.post('/api/reviews', reviewParams, { headers: context.rootState.userStore.headers })
-      .then(function(response){
-        context.commit('postReview', { newReview: response.data })
-      })
-      .catch(function (error) {
-        alert('予期しないエラーが発生しました。');
-      })
-    },
-
-    deleteReview(context, params){
-      axios.delete('/api/reviews/' + params.id,  { headers: context.rootState.userStore.headers })
-      .then(function(response){
-        context.commit('deleteReview', { review: response.data })
-      })
-      .catch(function (error) {
-        alert('予期しないエラーが発生しました。');
-      })
-    },
-
     postRoom(context, roomParams){
       axios.post('/api/powder_rooms', roomParams,  { headers: context.rootState.userStore.headers })
       .then(function(response){
@@ -162,5 +111,6 @@ export default {
         alert('予期しないエラーが発生しました。');
       })
     },
+    
   }
 }
