111841fe56f6ec7f8ff30b0ab3f15ae02fdb1314
Yukio Ugajin <jinugasachio@gmail.com>
2019-10-29 22:44:28 +0900

    add validation to facility.rb

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,3 +1,19 @@
 class Facility < ApplicationRecord
+  # Association
   belongs_to :powder_room
+
+  # Validation
+  validates :dresser,
+            :body_mirror,
+            :makeup_mirror,
+            :wifi,
+            :fitting_booth,
+            :washstands,
+            :luggage_storage,
+            :outlet,
+            :dust_box,
+            :waiting_space,
+            :others,
+            :membership,
+            :rate_plan, presence: true
 end
