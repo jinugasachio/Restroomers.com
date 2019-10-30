a7f6670c55643123e1afc2910632227179483356
Yukio Ugajin <jinugasachio@gmail.com>
2019-10-30 19:13:18 +0900

    add test to image_spec.rb

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :image do
-    
+    powder_room_id { 1 }
+    urls           { [File.open('db/images/pr1/pr.jpg')] }
   end
 end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Image, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    it '保存できる' do
+      expect(build(:image)).to be_valid
+    end
+
+    it 'urlsなしでは保存できない' do
+      image = build(:image, urls: nil)
+      image.valid?
+      expect(image.errors[:urls]).to include('を入力してください')
+    end
+
+    it 'powder_room_idなしでは保存できない' do
+      image = build(:image, powder_room_id: '')
+      image.valid?
+      expect(image.errors[:powder_room]).to include('を入力してください')
+    end
+  end
 end
