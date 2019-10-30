6e561432f9415f31dd8ef90d4ed6f880c072d583
Yukio Ugajin <jinugasachio@gmail.com>
2019-10-30 16:25:02 +0900

    add test to powder_room_spec.rb and detail_spec.rb

--- spec/factories/details.rb
+++ spec/factories/details.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :detail do
-    
+    open           { '10:00' }
+    close          { '21:00' }
+    phone_number   { '0345678901' }
+    address        { '東京都渋谷区宇田川町21-1 西武渋谷店A館 2F' }
+    official_url   { 'https://www.sogo-seibu.jp/shibuya/floor_guide/' }
+    powder_room_id { 1 }
   end
 end
--- spec/models/powder_room_spec.rb
+++ spec/models/powder_room_spec.rb
@@ -7,12 +7,41 @@ RSpec.describe PowderRoom, type: :model do
       expect(build(:powder_room)).to be_valid
     end
 
-    it '名前がないと投稿できない' do
+    it 'nameなしでは投稿できない' do
       room = build(:powder_room, name: '')
       room.valid?
-      expect(room.errors[:name]).to include('を入力してください。')
+      expect(room.errors[:name]).to include('を入力してください')
     end
 
+    it 'latなしでは投稿できない' do
+      room = build(:powder_room, lat: '')
+      room.valid?
+      expect(room.errors[:lat]).to include('を入力してください')
+    end
 
+    it 'lngなしでは投稿できない' do
+      room = build(:powder_room, lng: '')
+      room.valid?
+      expect(room.errors[:lng]).to include('を入力してください')
+    end
+
+    it 'user_idなしでは投稿できない' do
+      room = build(:powder_room, user: nil)
+      room.valid?
+      expect(room.errors[:user]).to include('を入力してください')
+    end
+
+    it 'latが数値以外では投稿できない' do
+      room = build(:powder_room, lat: 'アイウエオ')
+      room.valid?
+      expect(room.errors[:lat]).to include('は数値で入力してください')
+    end
+
+    it 'lngが数値以外では投稿できない' do
+      room = build(:powder_room, lng: 'アイウエオ')
+      room.valid?
+      expect(room.errors[:lng]).to include('は数値で入力してください')
+    end
   end
+  
 end
