175cd290885b9e187ff9dd26c2371608ae9c0523
Yukio Ugajin <jinugasachio@gmail.com>
2019-10-31 18:33:34 +0900

    fix api test in powder_rooms_spec.rb

--- app/controllers/api/powder_rooms_controller.rb
+++ app/controllers/api/powder_rooms_controller.rb
@@ -21,7 +21,7 @@ class Api::PowderRoomsController < ApplicationController
     room.build_facility(room_params[:facility_params])
     room.build_detail(room_params[:detail_params])
 
-    if room.save && room_params[:images_params][:urls].length > 0
+    if room.save
       image_files = []
       images_data = room_params[:images_params][:urls]
       images_data.each do |data|
