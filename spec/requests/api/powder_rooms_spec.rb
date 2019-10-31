3a08fe86c3a7de02cadbe9fa142b9499e0b1b0e4
Yukio Ugajin <jinugasachio@gmail.com>
2019-10-31 20:14:14 +0900

    fix api test in powder_rooms_spec.rb

--- spec/requests/api/powder_rooms_spec.rb
+++ spec/requests/api/powder_rooms_spec.rb
@@ -5,27 +5,27 @@ RSpec.describe 'PowderRoomsAPI', type: :request do
   let(:current_user) { create(:user) }
   let(:params) { attributes_for(:user) }
 
-  # it '全てのpowder_roomを取得する' do
-  #   FactoryBot.create_list(:powder_room, 10)
-  #   get '/api/powder_rooms'
-  #   json = JSON.parse(response.body)
-  #   expect(response.status).to eq(200)
-  #   expect(json.length).to eq(10)
-  # end
+  it '全てのpowder_roomを取得する' do
+    FactoryBot.create_list(:powder_room, 10)
+    get '/api/powder_rooms'
+    json = JSON.parse(response.body)
+    expect(response.status).to eq(200)
+    expect(json.length).to eq(10)
+  end
 
-  # it '特定のpowder_roomを取得する' do
-  #   room = create(:powder_room, name: 'テストルーム')
-  #   get "/api/powder_rooms/#{room.id}"
-  #   json = JSON.parse(response.body)
-  #   expect(response.status).to eq(200)
-  #   expect(json['powder_room']['name']).to eq(room.name)
-  # end
+  it '特定のpowder_roomを取得する' do
+    room = create(:powder_room, name: 'テストルーム')
+    get "/api/powder_rooms/#{room.id}"
+    json = JSON.parse(response.body)
+    expect(response.status).to eq(200)
+    expect(json['powder_room']['name']).to eq(room.name)
+  end
 
   it '新しいpowder_roomを作成する' do
-    room_params = { room: { room_params: { name: 'テストルーム', lat: 35.66019636, lng: 139.70036142 },
-                            images_params: { urls: [nil] }
-                          }
-                  }
+    room_params = { room: {
+      room_params:   { name: 'テストルーム', lat: 35.66019636, lng: 139.70036142 },
+      images_params: { urls: [nil] }
+    } }
     post(api_user_session_path, params: {
       email:    current_user.email,
       password: current_user.password
@@ -36,6 +36,7 @@ RSpec.describe 'PowderRoomsAPI', type: :request do
                     'access-token': response.headers['access-token']
                   })
     }.to change(PowderRoom, :count).by(+1)
+
     expect(response.status).to eq(200)
   end
 end
